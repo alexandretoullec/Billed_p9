cd5b888998de3b1a0d89a216ab0c972424fb3291
[ALexandre Toullec] <[alexandre.toullec@gmail.com]>
2023-09-14 12:55:45 +0200

    adding comments to code

--- src/app/Store.js
+++ src/app/Store.js
@@ -1,72 +1,142 @@
-
+// Function to handle API response and parse JSON, or throw an error if the response is not ok
 const jsonOrThrowIfError = async (response) => {
-  if(!response.ok) throw new Error((await response.json()).message)
-  return response.json()
-}
+  if (!response.ok) throw new Error((await response.json()).message);
+  return response.json();
+};
 
+// Class representing an API client with common HTTP methods
 class Api {
-  constructor({baseUrl}) {
+  constructor({ baseUrl }) {
     this.baseUrl = baseUrl;
   }
-  async get({url, headers}) {
-    return jsonOrThrowIfError(await fetch(`${this.baseUrl}${url}`, {headers, method: 'GET'}))
+
+  // Method for making a GET request
+  async get({ url, headers }) {
+    return jsonOrThrowIfError(
+      await fetch(`${this.baseUrl}${url}`, { headers, method: "GET" })
+    );
   }
-  async post({url, data, headers}) {
-    return jsonOrThrowIfError(await fetch(`${this.baseUrl}${url}`, {headers, method: 'POST', body: data}))
+
+  // Method for making a POST request
+  async post({ url, data, headers }) {
+    return jsonOrThrowIfError(
+      await fetch(`${this.baseUrl}${url}`, {
+        headers,
+        method: "POST",
+        body: data,
+      })
+    );
   }
-  async delete({url, headers}) {
-    return jsonOrThrowIfError(await fetch(`${this.baseUrl}${url}`, {headers, method: 'DELETE'}))
+
+  // Method for making a DELETE request
+  async delete({ url, headers }) {
+    return jsonOrThrowIfError(
+      await fetch(`${this.baseUrl}${url}`, { headers, method: "DELETE" })
+    );
   }
-  async patch({url, data, headers}) {
-    return jsonOrThrowIfError(await fetch(`${this.baseUrl}${url}`, {headers, method: 'PATCH', body: data}))
+
+  // Method for making a PATCH request
+  async patch({ url, data, headers }) {
+    return jsonOrThrowIfError(
+      await fetch(`${this.baseUrl}${url}`, {
+        headers,
+        method: "PATCH",
+        body: data,
+      })
+    );
   }
 }
 
+// Function to set headers for API requests, including Content-Type and Authorization if a JWT token exists
 const getHeaders = (headers) => {
-  const h = { }
-  if (!headers.noContentType) h['Content-Type'] = 'application/json'
-  const jwt = localStorage.getItem('jwt')
-  if (jwt && !headers.noAuthorization) h['Authorization'] = `Bearer ${jwt}`
-  return {...h, ...headers}
-}
+  const h = {};
+  if (!headers.noContentType) h["Content-Type"] = "application/json";
+  const jwt = localStorage.getItem("jwt");
+  if (jwt && !headers.noAuthorization) h["Authorization"] = `Bearer ${jwt}`;
+  return { ...h, ...headers };
+};
 
+// Class representing an entity in the API with common actions like select, list, update, create, and delete
 class ApiEntity {
-  constructor({key, api}) {
+  constructor({ key, api }) {
     this.key = key;
     this.api = api;
   }
-  async select({selector, headers = {}}) {
-    return await (this.api.get({url: `/${this.key}/${selector}`, headers: getHeaders(headers)}))
+
+  // Method to select a specific entity using a selector
+  async select({ selector, headers = {} }) {
+    return await this.api.get({
+      url: `/${this.key}/${selector}`,
+      headers: getHeaders(headers),
+    });
   }
-  async list({headers = {}} = {}) {
-    return await (this.api.get({url: `/${this.key}`, headers: getHeaders(headers)}))
+
+  // Method to list all entities of this type
+  async list({ headers = {} } = {}) {
+    return await this.api.get({
+      url: `/${this.key}`,
+      headers: getHeaders(headers),
+    });
   }
-  async update({data, selector, headers = {}}) {
-    return await (this.api.patch({url: `/${this.key}/${selector}`, headers: getHeaders(headers), data}))
+
+  // Method to update an entity
+  async update({ data, selector, headers = {} }) {
+    return await this.api.patch({
+      url: `/${this.key}/${selector}`,
+      headers: getHeaders(headers),
+      data,
+    });
   }
-  async create({data, headers = {}}) {
-    return await (this.api.post({url: `/${this.key}`, headers: getHeaders(headers), data}))
+
+  // Method to create a new entity
+  async create({ data, headers = {} }) {
+    return await this.api.post({
+      url: `/${this.key}`,
+      headers: getHeaders(headers),
+      data,
+    });
   }
-  async delete({selector, headers = {}}) {
-    return await (this.api.delete({url: `/${this.key}/${selector}`, headers: getHeaders(headers)}))
+
+  // Method to delete an entity
+  async delete({ selector, headers = {} }) {
+    return await this.api.delete({
+      url: `/${this.key}/${selector}`,
+      headers: getHeaders(headers),
+    });
   }
 }
 
-
-
+// Class representing a high-level interface to interact with the API
 class Store {
   constructor() {
-    this.api = new Api({baseUrl: 'http://localhost:5678'})
+    this.api = new Api({ baseUrl: "http://localhost:5678" }); // Initialize the API client with a base URL
   }
 
-  user = uid => (new ApiEntity({key: 'users', api: this.api})).select({selector: uid})
-  users = () => new ApiEntity({key: 'users', api: this.api})
-  login = (data) => this.api.post({url: '/auth/login', data, headers: getHeaders({noAuthorization: true})})
+  // Method to select a specific user
+  user = (uid) =>
+    new ApiEntity({ key: "users", api: this.api }).select({ selector: uid });
 
-  ref = (path) => this.store.doc(path)
+  // Method to list all users
+  users = () => new ApiEntity({ key: "users", api: this.api });
 
-  bill = bid => (new ApiEntity({key: 'bills', api: this.api})).select({selector: bid})
-  bills = () => new ApiEntity({key: 'bills', api: this.api})
+  // Method to send a login request
+  login = (data) =>
+    this.api.post({
+      url: "/auth/login",
+      data,
+      headers: getHeaders({ noAuthorization: true }),
+    });
+
+  // Method to reference a document (not defined in the provided code)
+  ref = (path) => this.store.doc(path);
+
+  // Method to select a specific bill
+  bill = (bid) =>
+    new ApiEntity({ key: "bills", api: this.api }).select({ selector: bid });
+
+  // Method to list all bills
+  bills = () => new ApiEntity({ key: "bills", api: this.api });
 }
 
-export default new Store()
+// Export an instance of the Store class to be used in other parts of the application
+export default new Store();
--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -1,60 +1,80 @@
-import { ROUTES_PATH } from '../constants/routes.js'
-import { formatDate, formatStatus } from "../app/format.js"
-import Logout from "./Logout.js"
+import { ROUTES_PATH } from "../constants/routes.js";
+import { formatDate, formatStatus } from "../app/format.js";
+import Logout from "./Logout.js";
 
 export default class {
   constructor({ document, onNavigate, store, localStorage }) {
-    this.document = document
-    this.onNavigate = onNavigate
-    this.store = store
-    const buttonNewBill = document.querySelector(`button[data-testid="btn-new-bill"]`)
-    if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
-    const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`)
-    if (iconEye) iconEye.forEach(icon => {
-      icon.addEventListener('click', () => this.handleClickIconEye(icon))
-    })
-    new Logout({ document, localStorage, onNavigate })
+    // Initialize class properties
+    this.document = document;
+    this.onNavigate = onNavigate;
+    this.store = store;
+
+    // Add click event listener to the "New Bill" button
+    const buttonNewBill = document.querySelector(
+      `button[data-testid="btn-new-bill"]`
+    );
+    if (buttonNewBill)
+      buttonNewBill.addEventListener("click", this.handleClickNewBill);
+
+    // Add click event listeners to bill icons (for viewing bill details)
+    const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`);
+    if (iconEye)
+      iconEye.forEach((icon) => {
+        icon.addEventListener("click", () => this.handleClickIconEye(icon));
+      });
+
+    // Initialize the logout feature
+    new Logout({ document, localStorage, onNavigate });
   }
 
+  // Handle click on the "New Bill" button
   handleClickNewBill = () => {
-    this.onNavigate(ROUTES_PATH['NewBill'])
-  }
+    this.onNavigate(ROUTES_PATH["NewBill"]);
+  };
 
+  // Handle click on the bill icon (for viewing bill details)
   handleClickIconEye = (icon) => {
-    const billUrl = icon.getAttribute("data-bill-url")
-    const imgWidth = Math.floor($('#modaleFile').width() * 0.5)
-    $('#modaleFile').find(".modal-body").html(`<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`)
-    $('#modaleFile').modal('show')
-  }
+    const billUrl = icon.getAttribute("data-bill-url");
+    console.log(billUrl);
+    const imgWidth = Math.floor($("#modaleFile").width() * 0.5);
+    $("#modaleFile")
+      .find(".modal-body")
+      .html(
+        `<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`
+      );
+    $("#modaleFile").modal("show");
+  };
 
+  // Get the list of bills from the store
   getBills = () => {
     if (this.store) {
       return this.store
-      .bills()
-      .list()
-      .then(snapshot => {
-        const bills = snapshot
-          .map(doc => {
+        .bills()
+        .list()
+        .then((snapshot) => {
+          // Format the bills and handle any potential errors
+          const bills = snapshot.map((doc) => {
             try {
               return {
                 ...doc,
                 date: formatDate(doc.date),
-                status: formatStatus(doc.status)
-              }
-            } catch(e) {
+                status: formatStatus(doc.status),
+              };
+            } catch (e) {
+              // If there are errors in formatting, log them and return unformatted data
               // if for some reason, corrupted data was introduced, we manage here failing formatDate function
               // log the error and return unformatted date in that case
-              console.log(e,'for',doc)
+              console.log(e, "for", doc);
               return {
                 ...doc,
                 date: doc.date,
-                status: formatStatus(doc.status)
-              }
+                status: formatStatus(doc.status),
+              };
             }
-          })
-          console.log('length', bills.length)
-        return bills
-      })
+          });
+          // console.log('length', bills.length)
+          return bills;
+        });
     }
-  }
+  };
 }
