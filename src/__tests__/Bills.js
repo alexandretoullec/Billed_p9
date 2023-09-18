ee206d48623fe8a07564c25ffbcebfb3c5c2dd42
[ALexandre Toullec] <[alexandre.toullec@gmail.com]>
2023-09-18 13:28:45 +0200

    adding integration test to Bill.js container

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,14 +2,14 @@
  * @jest-environment jsdom
  */
 
+import "@testing-library/jest-dom";
 import { screen, waitFor } from "@testing-library/dom";
-import BillsUI from "../views/BillsUI.js";
 import userEvent from "@testing-library/user-event";
-import { bills } from "../fixtures/bills.js";
 import Bills from "../containers/Bills.js";
+import BillsUI from "../views/BillsUI.js";
+import { bills } from "../fixtures/bills.js";
 import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
-
 import mockStore from "../__mocks__/store";
 import router from "../app/Router.js";
 
@@ -56,7 +56,7 @@ describe("Given I am connected as an employee", () => {
         document.body.innerHTML = ROUTES({ pathname });
       };
 
-      //Create an instance of th Bills class with necessary parameters
+      //Create an instance of the Bills class with necessary parameters
       const bills = new Bills({
         document,
         onNavigate,
@@ -81,6 +81,113 @@ describe("Given I am connected as an employee", () => {
 
       //check if the text "Send an expense note" is present in the DOM
       expect(screen.queryByText("Envoyer une note de frais")).toBeTruthy();
+    });
+  });
+
+  describe("when I click on th icon eye", () => {
+    test("it should render a modal", () => {
+      //define a function to simulate navigation by changing the page content
+      const onNavigate = (pathName) => {
+        document.body.innerHTML = ROUTES({ pathName });
+      };
+
+      //simulate BillsUI
+      document.body.innerHTML = BillsUI({ data: bills });
+
+      //Create an instance of the Bills Class with necessary parameters
+      const billsBis = new Bills({
+        document,
+        onNavigate,
+        localStorage: window.localStorage,
+      });
+
+      // Define a function to handle the click event for the "icon eye" button
+      const handleClickIconEye = jest.fn((icon) =>
+        billsBis.handleClickIconEye(icon)
+      );
+
+      // Select the modal element by its ID
+      const modaleFile = document.getElementById("modaleFile");
+
+      // Select all "icon eye" elements using their test id
+      const iconEye = screen.getAllByTestId("icon-eye");
+
+      // Mock the behavior of the $.fn.modal function to add the "show" class to the modal element
+      $.fn.modal = jest.fn(() => modaleFile.classList.add("show"));
+
+      // Add a click event listener to each "icon eye" element, calling handleClickIconEye when clicked
+      iconEye.forEach((icon) => {
+        icon.addEventListener("click", handleClickIconEye(icon));
+        userEvent.click(icon);
+        expect(handleClickIconEye).toHaveBeenCalled();
+      });
+
+      // Check if the modal element has the "show" class, indicating it is displayed
+      expect(modaleFile).toHaveClass("show");
+    });
+  });
+});
+
+// Test d'intégration GET
+describe("Given when I am a user connected as Admin", () => {
+  describe("When I navigate to Bills", () => {
+    test("Then fetches bills from mock API GET", async () => {
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ type: "Admin", email: "a@a" })
+      );
+      new Bills({
+        document,
+        onNavigate,
+        mockStore,
+        localStorage: window.localStorage,
+      });
+      document.body.innerHTML = BillsUI({ data: bills });
+      await waitFor(() => screen.getByText("Mes notes de frais"));
+      expect(screen.getByText("Mes notes de frais")).toBeTruthy();
+    });
+  });
+
+  describe("When an error occurs on API", () => {
+    beforeEach(() => {
+      jest.spyOn(mockStore, "bills");
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Admin",
+          email: "a@a",
+        })
+      );
+    });
+    test("should fetch and fail with 404 message error", async () => {
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          list: () => {
+            return Promise.reject(new Error("Erreur 404"));
+          },
+        };
+      });
+      const html = BillsUI({ error: "Erreur 404" });
+      document.body.innerHTML = html;
+      const message = await screen.getByText(/Erreur 404/);
+      expect(message).toBeTruthy();
+    });
+
+    test("should fetch and fail with 500 message error", async () => {
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          list: () => {
+            return Promise.reject(new Error("Erreur 500"));
+          },
+        };
+      });
+      const html = BillsUI({ error: "Erreur 500" });
+      document.body.innerHTML = html;
+      const message = await screen.getByText(/Erreur 500/);
+      expect(message).toBeTruthy();
     });
   });
 });
